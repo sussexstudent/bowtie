d454f70e2a46135a577c80376a8f884d3b40f1bd
James Canning <james@brudil.com>
2019-08-01 15:21:08 +0100

    Add prettier

--- src/keys.ts
+++ src/keys.ts
@@ -1,26 +1,28 @@
 import * as AWS from 'aws-sdk';
 import * as SSM from 'aws-sdk/clients/ssm';
 
-AWS.config.update({ region: "eu-west-1" });
+AWS.config.update({ region: 'eu-west-1' });
 
-const ssm = new SSM()
+const ssm = new SSM();
 
-export const loadParams = async (names: string[]): Promise<Map<string, SSM.Parameter>> => {
-    const query = {
-        "Names": names,
-        "WithDecryption": true
-    }
-    const res = await ssm.getParameters(query).promise()
+export const loadParams = async (
+  names: string[],
+): Promise<Map<string, SSM.Parameter>> => {
+  const query = {
+    Names: names,
+    WithDecryption: true,
+  };
+  const res = await ssm.getParameters(query).promise();
 
-    if (res.Parameters) {
-        const map = res.Parameters.reduce((m, param) => {
-            m.set(param.Name, param);
-    
-            return m;
-        }, new Map())
-    
-        return map;
-    }
+  if (res.Parameters) {
+    const map = res.Parameters.reduce((m, param) => {
+      m.set(param.Name, param);
 
-    throw new Error('key access failed');
+      return m;
+    }, new Map());
+
+    return map;
+  }
+
+  throw new Error('key access failed');
 };
